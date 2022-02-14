a8049b731d500c09c6d1d726d4d4218f07924640
Daniel Tesfai <dannyhd88@gmail.com>
2022-02-14 14:34:55 +0300

    reached to list details and update level

--- appToDo_woo/urls.py
+++ appToDo_woo/urls.py
@@ -1,12 +1,15 @@
 import django
 from django.urls import URLPattern, path
 from appToDo_woo import views
+from .models import Todo
 
 #create your patterns here
 
 urlpatterns=[
     path('signUp/',views.signUpUser,name="signUp"),
     path('home/',views.home,name="home"),
+    path('home/<int:pk_Todo>',views.todo_detail,name="detail"),
+    path('create/',views.new_todo,name="create_new"),
     path('logout/',views.logoutUser,name="logoutUser"),
     path('login/',views.loginUser,name="loginUser")
 ]
--- appToDo_woo/views.py
+++ appToDo_woo/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .models import create_new_form,Todo
 
 # Create your views here.
 
@@ -23,7 +24,8 @@ def signUpUser(request):
                 # let the user now either the username or password is wrong
                 return render(request,'appToDo_woo/signUp.html',{'signUpForm':UserCreationForm(),'error':'password didnt match'})
 def home(request):
-    return render(request,'appToDo_woo/home.html')
+    todo_lists=Todo.objects.filter(user=request.user)
+    return render(request,'appToDo_woo/home.html',{'todoLists':todo_lists})
 
 def logoutUser(request):
     if request.method=='POST':
@@ -40,7 +42,42 @@ def loginUser(request):
         else:
             login(request,user)
             return redirect('home')
+
+def new_todo(request):
+    if request.method=='GET':
+        return render(request,'appToDo_woo/create_new.html',{'create_new_form':create_new_form()})
+    else:
+        try:
+            new_todo_form = create_new_form(request.POST)
+            new_to_do=new_todo_form.save(commit=False)
+            new_to_do.user=request.user
+            new_to_do.save()
+            return redirect('home')
+
+        except ValueError:
+            return render(request,'appToDo_woo/create_new.html',{'create_new_form':create_new_form(),'error':'bad data type or exceeded the length specified of a specific field .'})
+
+
+
+def todo_detail(request,pk_Todo):
+    to_detail=get_object_or_404(Todo,pk=pk_Todo)
+    if request.method=='GET':
+        detailForm=create_new_form(instance=to_detail)
+        return render(request,'appToDo_woo/detail.html',{'deta':to_detail , 'details_form':detailForm})
+    else:
+        try:
+            detailForm = create_new_form(request.POST,instance=to_detail) 
+            detailForm.save()
+            return redirect('home')
+        except ValueError:
+              return render(request,'appToDo_woo/detail.html',{'deta':to_detail , 'details_form':detailForm ,'error':'bad data type or exceeded the length specified of a specific field .'})
+
+
+
+             
+
             
+
 
 
 
