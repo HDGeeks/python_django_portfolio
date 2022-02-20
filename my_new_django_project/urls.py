cca072c2cd5404161f3081a7998994397042be45
Daniel Tesfai <dannyhd88@gmail.com>
2022-02-20 13:50:09 +0300

    prettier configured

--- my_new_django_project/urls.py
+++ my_new_django_project/urls.py
@@ -24,12 +24,19 @@ from django.contrib.auth import views as auth_view
 
 urlpatterns = [ 
     path('register/',users_view.register,name='register'),
+
     path('login/',auth_view.LoginView.as_view(template_name='users_app/login.html'),name='login'),
+
     path('logout',auth_view.LogoutView.as_view(template_name='users_app/logout.html'),name='logout'),
+
     path('password_reset',auth_view.PasswordResetView.as_view(template_name='users_app/password_reset.html'),name='password_reset'),
+
     path('password_reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='users_app/password_reset_done.html'),name='password_reset_done'),
+
     path('password_reset_confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='users_app/password_reset_confirm.html'),name='password_reset_confirm'),
-     path('password_reset/complete/',auth_view.PasswordResetCompleteView.as_view(template_name='users_app/password_reset_complete.html'),name='password_reset_complete'),
+    
+    path('password_reset/complete/',auth_view.PasswordResetCompleteView.as_view(template_name='users_app/password_reset_complete.html'),name='password_reset_complete'),
+
     path('profile/',users_view.profile,name='profile'),
     path('blog/',include('blog.urls')),
     path('polls/',include('app_polls.urls')),
